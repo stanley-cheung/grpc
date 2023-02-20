eccf7e9513d98f3ca77f0db46bf55f2dfbe7b921
Stanley Cheung <stanleycheung@google.com>
2023-02-19 17:02:39 -0800

    clean up

--- test/cpp/interop/interop_client.cc
+++ test/cpp/interop/interop_client.cc
@@ -705,13 +705,11 @@ bool InteropClient::DoPingPong() {
     response_parameter->set_size(response_stream_sizes[i]);
     payload->set_body(std::string(request_stream_sizes[i], '\0'));
 
-    gpr_log(GPR_DEBUG, "Client: writing a message in FullDuplexCall");
     if (!stream->Write(request)) {
       gpr_log(GPR_ERROR, "DoPingPong(): stream->Write() failed. i: %d", i);
       return TransientFailureOrAbort();
     }
 
-    gpr_log(GPR_DEBUG, "Client: reading a message in FullDuplexCall");
     if (!stream->Read(&response)) {
       gpr_log(GPR_ERROR, "DoPingPong(): stream->Read() failed. i:%d", i);
       return TransientFailureOrAbort();
