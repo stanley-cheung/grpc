10eeb716cbb5a7c82cc0660a3f1633cefd546425
Stanley Cheung <stanleycheung@google.com>
2023-02-23 12:10:37 -0800

    remove sleep seconds param from framework

--- tools/dockerfile/observability-test/cpp/run.sh
+++ tools/dockerfile/observability-test/cpp/run.sh
@@ -24,8 +24,7 @@ cd "$(dirname "$0")"/../..
 #
 # For client: $2: server_host
 #             $3: server_port
-#             $4: observability_exporter_sleep_seconds
-#             $5: test_case
+#             $4: test_case    # comma delimited interop test case names
 
 if [ "$1" = "server" ] ; then
   /grpc/bazel-bin/test/cpp/interop/interop_server \
@@ -35,7 +34,7 @@ elif [ "$1" = "client" ] ; then
   /grpc/bazel-bin/test/cpp/interop/interop_client \
     --enable_observability=true \
     --server_host=$2 --server_port=$3 \
-    --observability_exporter_sleep_seconds=$4 --test_case=$5
+    --test_case=$4
 
 else
   echo "Invalid action: $1"
