84d510d37ea9715718a8b61e4cb575a329d92ac8
stanley-cheung <stanley-cheung@users.noreply.github.com>
2023-02-28 23:15:15 +0000

    Automated change: Fix sanity tests

--- test/cpp/interop/interop_server_bootstrap.cc
+++ test/cpp/interop/interop_server_bootstrap.cc
@@ -16,17 +16,20 @@
 //
 //
 
-#include <grpcpp/ext/gcp_observability.h>
 #include <signal.h>
 
 #include "absl/flags/flag.h"
+
+#include <grpcpp/ext/gcp_observability.h>
+
 #include "test/core/util/test_config.h"
 #include "test/cpp/interop/server_helper.h"
 #include "test/cpp/util/test_config.h"
 
 gpr_atm grpc::testing::interop::g_got_sigint;
 
-ABSL_FLAG(bool, enable_observability, false, "Whether to enable GCP Observability");
+ABSL_FLAG(bool, enable_observability, false,
+          "Whether to enable GCP Observability");
 
 static void sigint_handler(int /*x*/) {
   gpr_atm_no_barrier_store(&grpc::testing::interop::g_got_sigint, true);
