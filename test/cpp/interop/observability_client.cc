4ba8268ca5122ee5a1ff265bdeb8677e9efc6ca8
Stanley Cheung <stanleycheung@google.com>
2023-02-28 23:35:34 -0800

    refactor flags

--- test/cpp/interop/observability_client.cc
+++ test/cpp/interop/observability_client.cc
@@ -35,55 +35,6 @@
 #include "test/cpp/interop/interop_client.h"
 #include "test/cpp/util/test_config.h"
 
-ABSL_FLAG(bool, use_tls, false, "Whether to use tls.");
-ABSL_FLAG(std::string, custom_credentials_type, "",
-          "User provided credentials type.");
-ABSL_FLAG(bool, use_test_ca, false, "False to use SSL roots for google");
-ABSL_FLAG(int32_t, server_port, 0, "Server port.");
-ABSL_FLAG(std::string, server_host, "localhost", "Server host to connect to");
-ABSL_FLAG(std::string, server_host_override, "",
-          "Override the server host which is sent in HTTP header");
-ABSL_FLAG(int32_t, num_times, 1, "Number of times to run the test case");
-ABSL_FLAG(std::string, default_service_account, "",
-          "Email of GCE default service account");
-ABSL_FLAG(std::string, service_account_key_file, "",
-          "Path to service account json key file.");
-ABSL_FLAG(std::string, oauth_scope, "", "Scope for OAuth tokens.");
-ABSL_FLAG(bool, do_not_abort_on_transient_failures, false,
-          "If set to 'true', abort() is not called in case of transient "
-          "failures (i.e failures that are temporary and will likely go away "
-          "on retrying; like a temporary connection failure) and an error "
-          "message is printed instead. Note that this flag just controls "
-          "whether abort() is called or not. It does not control whether the "
-          "test is retried in case of transient failures (and currently the "
-          "interop tests are not retried even if this flag is set to true)");
-ABSL_FLAG(int32_t, soak_iterations, 1000,
-          "The number of iterations to use for the two soak tests; rpc_soak "
-          "and channel_soak.");
-ABSL_FLAG(int32_t, soak_max_failures, 0,
-          "The number of iterations in soak tests that are allowed to fail "
-          "(either due to non-OK status code or exceeding the "
-          "per-iteration max acceptable latency).");
-ABSL_FLAG(int32_t, soak_per_iteration_max_acceptable_latency_ms, 0,
-          "The number of milliseconds a single iteration in the two soak "
-          "tests (rpc_soak and channel_soak) should take.");
-ABSL_FLAG(int32_t, soak_overall_timeout_seconds, 0,
-          "The overall number of seconds after which a soak test should "
-          "stop and fail, if the desired number of iterations have not yet "
-          "completed.");
-ABSL_FLAG(int32_t, soak_min_time_ms_between_rpcs, 0,
-          "The minimum time in milliseconds between consecutive RPCs in a "
-          "soak test (rpc_soak or channel_soak), useful for limiting QPS");
-ABSL_FLAG(int32_t, iteration_interval, 10,
-          "The interval in seconds between rpcs. This is used by "
-          "long_connection test");
-ABSL_FLAG(std::string, additional_metadata, "",
-          "Additional metadata to send in each request, as a "
-          "semicolon-separated list of key:value pairs.");
-ABSL_FLAG(
-    bool, log_metadata_and_status, false,
-    "If set to 'true', will print received initial and trailing metadata, "
-    "grpc-status and error message to the console, in a stable format.");
 ABSL_FLAG(bool, enable_observability, false,
           "Whether to enable GCP Observability");
 
