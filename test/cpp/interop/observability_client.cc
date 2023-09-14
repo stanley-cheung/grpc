fd7d8369aabdbe6447d0d8f1e5725d92d2ae145a
stanley-cheung <stanley-cheung@users.noreply.github.com>
2023-09-14 19:54:42 +0000

    Automated change: Fix sanity tests

--- test/cpp/interop/observability_client.cc
+++ test/cpp/interop/observability_client.cc
@@ -20,6 +20,10 @@
 #include <unordered_map>
 
 #include "absl/flags/flag.h"
+#include "opentelemetry/exporters/prometheus/exporter_factory.h"
+#include "opentelemetry/exporters/prometheus/exporter_options.h"
+#include "opentelemetry/sdk/metrics/meter_provider.h"
+#include "opentelemetry/sdk/metrics/meter_provider_factory.h"
 
 #include <grpc/grpc.h>
 #include <grpc/support/alloc.h>
@@ -28,10 +32,6 @@
 #include <grpcpp/client_context.h>
 #include <grpcpp/ext/gcp_observability.h>
 
-#include "opentelemetry/exporters/prometheus/exporter_factory.h"
-#include "opentelemetry/exporters/prometheus/exporter_options.h"
-#include "opentelemetry/sdk/metrics/meter_provider_factory.h"
-#include "opentelemetry/sdk/metrics/meter_provider.h"
 #include "src/core/lib/gpr/string.h"
 #include "src/core/lib/gprpp/crash.h"
 #include "src/cpp/ext/otel/otel_plugin.h"
@@ -213,20 +213,23 @@ int main(int argc, char** argv) {
 
   if (absl::GetFlag(FLAGS_enable_observability)) {
     gpr_log(GPR_DEBUG, "client Skipping old Gcp observabilty init");
-    //auto status = grpc::experimental::GcpObservabilityInit();
-    //gpr_log(GPR_DEBUG, "GcpObservabilityInit() status_code: %d", status.code());
-    //if (!status.ok()) {
-    //  return 1;
-    //}
+    // auto status = grpc::experimental::GcpObservabilityInit();
+    // gpr_log(GPR_DEBUG, "GcpObservabilityInit() status_code: %d",
+    // status.code()); if (!status.ok()) {
+    //   return 1;
+    // }
   }
 
   gpr_log(GPR_DEBUG, "registering prometheus exporter");
   opentelemetry::exporter::metrics::PrometheusExporterOptions opts;
-  auto prometheus_exporter = opentelemetry::exporter::metrics::PrometheusExporterFactory::Create(opts);
+  auto prometheus_exporter =
+      opentelemetry::exporter::metrics::PrometheusExporterFactory::Create(opts);
   auto u_provider = opentelemetry::sdk::metrics::MeterProviderFactory::Create();
-  auto *p         = static_cast<opentelemetry::sdk::metrics::MeterProvider *>(u_provider.get());
+  auto* p = static_cast<opentelemetry::sdk::metrics::MeterProvider*>(
+      u_provider.get());
   p->AddMetricReader(std::move(prometheus_exporter));
-  std::shared_ptr<opentelemetry::metrics::MeterProvider> meter_provider(std::move(u_provider));
+  std::shared_ptr<opentelemetry::metrics::MeterProvider> meter_provider(
+      std::move(u_provider));
   grpc::internal::OpenTelemetryPluginBuilder ot_builder;
   ot_builder.SetMeterProvider(std::move(meter_provider));
   ot_builder.BuildAndRegisterGlobal();
@@ -388,7 +391,7 @@ int main(int argc, char** argv) {
   sleep(60);
 
   if (absl::GetFlag(FLAGS_enable_observability)) {
-    //grpc::experimental::GcpObservabilityClose();
+    // grpc::experimental::GcpObservabilityClose();
   }
 
   return ret;
